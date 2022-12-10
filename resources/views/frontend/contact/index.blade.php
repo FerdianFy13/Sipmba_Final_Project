f6e5929eae94e91e769cc4add2e206611123f575
FerdianFy13 <ferdianfy13@gmail.com>
2022-12-10 19:44:58 +0700

    update ui contact logic query in frontend

--- resources/views/frontend/contact/index.blade.php
+++ resources/views/frontend/contact/index.blade.php
@@ -55,8 +55,9 @@
                         <div class="info-item d-flex">
                             <i class="bi bi-geo-alt flex-shrink-0"></i>
                             <div>
-                                <h4>Location:</h4>
-                                <p>A108 Adam Street, New York, NY 535022</p>
+                                <h4>Lokasi:</h4>
+                                <p>Jalan Jend. Sudirman No.1, Babadan, Bantul, Kec. Bantul, Kabupaten Bantul, Daerah
+                                    Istimewa Yogyakarta 55711</p>
                             </div>
                         </div><!-- End Info Item -->
 
@@ -64,23 +65,23 @@
                             <i class="bi bi-envelope flex-shrink-0"></i>
                             <div>
                                 <h4>Email:</h4>
-                                <p>info@example.com</p>
+                                <p>pmikab.bantul@gmail.com</p>
                             </div>
                         </div><!-- End Info Item -->
 
                         <div class="info-item d-flex">
                             <i class="bi bi-phone flex-shrink-0"></i>
                             <div>
-                                <h4>Call:</h4>
-                                <p>+1 5589 55488 55</p>
+                                <h4>Telepon:</h4>
+                                <p>(0274) 367987</p>
                             </div>
                         </div><!-- End Info Item -->
 
                         <div class="info-item d-flex">
                             <i class="bi bi-clock flex-shrink-0"></i>
                             <div>
-                                <h4>Open Hours:</h4>
-                                <p>Mon-Sat: 11AM - 23PM</p>
+                                <h4>Jam Buka:</h4>
+                                <p>Senin-Minggu: 24 Jam</p>
                             </div>
                         </div><!-- End Info Item -->
                     </div>
@@ -88,35 +89,38 @@
                 </div>
 
                 <div class="col-lg-8">
-                    <form action="/kontak" method="post" enctype="multipart/form-data" class="php-email-form">
-                        @csrf
-                        <h4 class="fw-bold">Hubungi Kami,</h4>
-                        <div class="row mt-4">
-                            <div class="col-md-6 form-group">
-                                <input type="text" name="name" class="form-control" id="name"
-                                    placeholder="Nama Pengirim" required>
+                    <h4 class="fw-bold ms-5 mt-3">Hubungi Kami,</h4>
+                    <div class="card ms-5">
+                        <form action="/kontak" method="post" enctype="multipart/form-data">
+                            @csrf
+                            <div class="row mt-4">
+                                <div class="col-md-6 form-group">
+                                    <input type="text" name="name" class="form-control" id="name"
+                                        placeholder="Nama Pengirim" required>
+                                </div>
+                                <div class="col-md-6 form-group mt-3 mt-md-0">
+                                    <input type="email" class="form-control" name="email" id="email"
+                                        placeholder="Alamat Email" required>
+                                </div>
                             </div>
-                            <div class="col-md-6 form-group mt-3 mt-md-0">
-                                <input type="email" class="form-control" name="email" id="email"
-                                    placeholder="Alamat Email" required>
+                            <div class="form-group mt-3">
+                                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subjek"
+                                    required>
                             </div>
-                        </div>
-                        <div class="form-group mt-3">
-                            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subjek"
-                                required>
-                        </div>
-                        <div class="form-group mt-3">
-                            <textarea class="form-control" name="message" rows="5" placeholder="Pesan"
-                                required></textarea>
-                        </div>
-                        <div class="my-3">
-                            <div class="loading">Loading</div>
-                            <div class="error-message"></div>
-                            <div class="sent-message">Your message has been sent. Thank you!</div>
-                        </div>
-                        <div class="text-center"><button type="submit">Send Message</button></div>
-                        {{-- <button type="submit" class="text-center justify-content-center">Kirim Pesan</button> --}}
-                    </form>
+                            <div class="form-group mt-3">
+                                <textarea class="form-control" name="message" rows="5" placeholder="Pesan"
+                                    required></textarea>
+                            </div>
+                            {{-- <div class="my-3">
+                                <div class="loading">Loading</div>
+                                <div class="error-message"></div>
+                                <div class="sent-message">Your message has been sent. Thank you!</div>
+                            </div> --}}
+                            <div class="text-center mt-4"><button type="submit" class="btn btn-danger">Kirim Pesan</button></div>
+                            {{-- <button type="submit" class="text-center justify-content-center">Kirim Pesan</button>
+                            --}}
+                        </form>
+                    </div>
                 </div><!-- End Contact Form -->
             </div>
 
