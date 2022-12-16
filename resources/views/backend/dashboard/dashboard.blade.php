0ae3c28743c60240d8e04c1081a4ad859d8c5dd7
FerdianFy13 <ferdianfy13@gmail.com>
2022-12-16 21:41:28 +0700

    update add logic query bar chart blood stock

--- resources/views/backend/dashboard/dashboard.blade.php
+++ resources/views/backend/dashboard/dashboard.blade.php
@@ -33,7 +33,7 @@
     <div class="col">
         <div class="card h-70">
             <div class="card-body">
-                <h5 class="card-title text-center mt-3">Stok Darah</h5>
+                <h4 class="card-title text-center mt-3">Stok Darah</h4>
                 <div class="row row-cols-1 row-cols-md-4 g-4 mt-1">
                     <div class="col text-center">
                         <div class="card h-50">
@@ -75,7 +75,7 @@
     <div class="col">
         <div class="card h-70">
             <div class="card-body">
-                <h5 class="card-title text-center mt-3">Tampilan</h5>
+                <h4 class="card-title text-center mt-3">Tampilan</h4>
                 <div class="row row-cols-1 row-cols-md-2 g-4 mt-1">
                     <div class="col text-center">
                         <div class="card h-50">
@@ -98,4 +98,70 @@
         </div>
     </div>
 </div>
+<div class="row row-cols-1 row-cols-md-2 g-4">
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title text-center mt-3">Grafik Jumlah Darah Masuk</h5>
+                <div id="piechart_3d" class="text-center justify-content-center mt-5"
+                    style="width: 400px; height: 300px;">
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title text-center mt-3">Grafik Jumlah Darah Keluar</h5>
+                <div id="piechart_3d2" class="text-center justify-content-center mt-5"
+                    style="width: 400px; height: 300px;">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+    google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Blood Type', 'Result'],
+          ['A',     <?= $bloodTypeA; ?>],
+          ['B',      <?= $bloodTypeB ?>],
+          ['AB',    <?= $bloodTypeAB; ?>],
+          ['0',     <?= $bloodTypeO; ?>],
+        ]);
+
+        var options = {
+          title: 'Stok Darah',
+          is3D: true,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+        chart.draw(data, options);
+      }
+</script>
+<script type="text/javascript">
+    google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Blood Type', 'Result'],
+          ['A',     11],
+          ['B',      2],
+          ['AB',  2],
+          ['O', 2],
+        ]);
+
+        var options = {
+          title: 'Permintaan Darah',
+          is3D: true,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d2'));
+        chart.draw(data, options);
+      }
+</script>
